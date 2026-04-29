6cfaeb38e65749539141afaad84c1615a8ed58ff
jsantiagodaza <jsantiagodaza@unicesar.edu.co>
2026-04-29 08:22:31 -0500

    feat: En la capa Model, se le agregaron los atributos iniciales a la c}lase Estación

--- src/vektra/Model/Estacion.java
+++ src/vektra/Model/Estacion.java
@@ -3,11 +3,22 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package vektra.Model;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
  * @author santi
  */
 public class Estacion {
+    private String id;
+    private String nombre;
+    private List<String> lineas; //Colores de las Líneas que pasan
+
+    public Estacion(String id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+        this.lineas = new ArrayList<>();
+    }
     
 }
