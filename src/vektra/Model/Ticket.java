d9e116c91040bcbe41842e6a6660d252e2890f88
jsantiagodaza <jsantiagodaza@unicesar.edu.co>
2026-04-29 08:30:23 -0500

    feat: En la capa Model, se le agregaron los atributos iniciales a la clase Ticket

--- src/vektra/Model/Ticket.java
+++ src/vektra/Model/Ticket.java
@@ -4,10 +4,25 @@
  */
 package vektra.Model;
 
+import java.time.LocalDateTime;
+
 /**
  *
  * @author santi
  */
 public class Ticket {
+    
+     private String id;
+    private LocalDateTime fecha;
+    private double precio;
+    private String codigoQR;
+
+    public Ticket(String id, LocalDateTime fecha, double precio, String codigoQR) {
+        this.id = id;
+        this.fecha = fecha;
+        this.precio = precio;
+        this.codigoQR = codigoQR;
+    }
+    
     
 }
