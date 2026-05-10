15fd2eef743c67edfdee7608805ceb2f7108e370
https://github.com/sebastianalopez-a11y <Sebastianalopez@unicesar.edu.co>
2026-05-10 15:57:21 -0500

    feat: se creo el metodo setter en la clase estacion

--- src/vektra/Model/Estacion.java
+++ src/vektra/Model/Estacion.java
@@ -27,14 +27,30 @@ public class Estacion {
     public String getNombre() {
         return nombre;
     }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 
     public String getId() {
         return id;
+    }
+     public void setId(String id) {
+        this.id = id;
     }
 
     public List<String> getLineas() {
         return lineas;
     }
+    public void setLineas(List<String> lineas) {
+        this.lineas = lineas;
+    }
     
-    
+    @Override
+    public String toString(){
+        return "Estacion{" +
+                "id='" + id + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", color linea='"+ lineas +'\''+
+                '}'; 
+    }
 }
