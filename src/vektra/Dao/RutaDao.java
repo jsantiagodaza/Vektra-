dc3be2f5245c33de2e0fe78874abb115795f0eea
https://github.com/sebastianalopez-a11y <Sebastianalopez@unicesar.edu.co>
2026-05-10 14:16:44 -0500

    feat: se creo el metodo guardar ruta

--- src/vektra/Dao/RutaDao.java
+++ src/vektra/Dao/RutaDao.java
@@ -4,10 +4,30 @@
  */
 package vektra.Dao;
 
+import java.util.ArrayList;
+import java.util.List;
+import vektra.Model.Ruta;
+import java.util.Map;
+import java.util.HashMap;
 /**
  *
  * @author santi
  */
 public class RutaDao {
+    private List<Ruta> rutas;
+    private Map<String, List<String>> conexiones;
+    
+    public RutaDao(){
+        rutas = new ArrayList<>();
+        conexiones = new HashMap<>();
+    }
+    public void GuardarRuta(Ruta ruta){
+        rutas.add(ruta);
+        construirConexion(ruta);
+    }
+    
+    private void construirConexion(Ruta ruta){
+        
+    }
     
 }
