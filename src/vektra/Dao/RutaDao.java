10a20eec04b7318b2ef129b4934f1bb83290d062
https://github.com/sebastianalopez-a11y <Sebastianalopez@unicesar.edu.co>
2026-05-10 15:35:22 -0500

    feat: se creo el metodo crearRuta

--- src/vektra/Dao/RutaDao.java
+++ src/vektra/Dao/RutaDao.java
@@ -10,49 +10,50 @@ import vektra.Model.Ruta;
 import java.util.Map;
 import java.util.HashMap;
 import vektra.Model.Estacion;
+
 /**
  *
  * @author santi
  */
 public class RutaDao {
+
     private List<Ruta> rutas;
     private Map<String, List<String>> conexiones;
-    
-    public RutaDao(){
+
+    public RutaDao() {
         rutas = new ArrayList<>();
         conexiones = new HashMap<>();
     }
-    public void GuardarRuta(Ruta ruta){
+
+    public void GuardarRuta(Ruta ruta) {
         rutas.add(ruta);
         construirConexion(ruta);
     }
-    
-    public List<Ruta>ObtenerRutas(){
+
+    public List<Ruta> ObtenerRutas() {
         return rutas;
     }
-            
-    
-    private void construirConexion(Ruta ruta){
+
+    private void construirConexion(Ruta ruta) {
         List<String> estaciones = new ArrayList<>();
         estaciones.add(ruta.getOrigen().getNombre());
-        
-        for(Estacion e : ruta.getEstacionesIntermedias()){
-           estaciones.add(e.getNombre());
+
+        for (Estacion e : ruta.getEstacionesIntermedias()) {
+            estaciones.add(e.getNombre());
         }
         estaciones.add(ruta.getDestino().getNombre());
-        for (int i =0; i < estaciones.size()-1 ; i++){
+        for (int i = 0; i < estaciones.size() - 1; i++) {
             String actual = estaciones.get(i);
-            String siguiente = estaciones.get(i+ 1);
-            agregarConexion (actual,siguiente);
+            String siguiente = estaciones.get(i + 1);
+            agregarConexion(actual, siguiente);
         }
     }
-    
-    private void agregarConexion(String origen, String destino){
-        
+
+    private void agregarConexion(String origen, String destino) {
+
         conexiones.putIfAbsent(origen, new ArrayList<>());
         conexiones.putIfAbsent(destino, new ArrayList<>());
         conexiones.get(origen).add(destino);
         conexiones.get(destino).add(origen);
     }
-        
 }
