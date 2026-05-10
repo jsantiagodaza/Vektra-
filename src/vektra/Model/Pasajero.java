fc7e1ed60fb457f5e2c08422442d7e91d8a21fa6
xacosta <xacosta@unicesar.edu.co>
2026-05-10 00:18:03 -0500

    feat: se agregó el atributo contraseña, los constructores, gett y sett

--- src/vektra/Model/Pasajero.java
+++ src/vektra/Model/Pasajero.java
@@ -4,17 +4,53 @@
  */
 package vektra.Model;
 
+import java.time.LocalDateTime;
+
 /**
  *
  * @author santi
  */
 public class Pasajero extends Persona {
     private String email;
+    private String contraseña;
+    private LocalDateTime fechaRegistro;
 
-    public Pasajero(String email, String id, String nombre) {
+    public Pasajero() {
+    }
+
+    public Pasajero(String email, String contraseña, LocalDateTime fechaRegistro, String id, String nombre) {
         super(id, nombre);
         this.email = email;
+        this.contraseña = contraseña;
+        this.fechaRegistro = fechaRegistro;
     }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    public LocalDateTime getFechaRegistro() {
+        return fechaRegistro;
+    }
+
+    public void setFechaRegistro(LocalDateTime fechaRegistro) {
+        this.fechaRegistro = fechaRegistro;
+    }
+
+    
+  
 
    
     
