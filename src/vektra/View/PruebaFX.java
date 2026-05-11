1c97da317ddbef9688e5ba10c3e81b56be3b26b6
https://github.com/sebastianalopez-a11y <Sebastianalopez@unicesar.edu.co>
2026-05-11 12:50:29 -0500

    feat: se trabajo en la clase prueba fx

--- src/vektra/View/PruebaFX.java
+++ src/vektra/View/PruebaFX.java
@@ -1,13 +1,28 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package vektra.View;
 
-/**
- *
- * @author Usuario
- */
-public class PruebaFX {
-    
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class PruebaFX extends Application {
+
+    @Override
+    public void start(Stage stage) {
+
+        Label label = new Label("JavaFX funcionando");
+
+        Scene scene = new Scene(label, 400, 200);
+
+        stage.setScene(scene);
+
+        stage.setTitle("Prueba JavaFX");
+
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
+
