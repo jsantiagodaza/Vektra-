42c3ef1812f9c1f8b348e110295fdd84b1ccc491
https://github.com/sebastianalopez-a11y <Sebastianalopez@unicesar.edu.co>
2026-05-22 14:57:39 -0500

    se crearon los metodos getter y setter de la clase usuario

--- src/vektra/Model/Usuario.java
+++ src/vektra/Model/Usuario.java
@@ -9,14 +9,52 @@ package vektra.Model;
  * @author Usuario
  */
 public class Usuario {
-    
+
     private String idUsuario;
     private String nombre;
     private String password;
     private String rol;
-    
-    public Usuario(){
+
+    public Usuario() {
     }
-    
-    
+
+    public Usuario(String idUsuario, String nombre, String password, String rol) {
+        this.idUsuario = idUsuario;
+        this.nombre = nombre;
+        this.password = password;
+        this.rol = rol;
+    }
+
+    public String getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(String idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
 }
