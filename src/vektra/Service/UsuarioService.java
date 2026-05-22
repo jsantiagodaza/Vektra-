9a20a5c5148e48fc28318efae24f95a061b01fb0
https://github.com/sebastianalopez-a11y <Sebastianalopez@unicesar.edu.co>
2026-05-22 16:02:28 -0500

    se creo la validacion de los campos id y password

--- src/vektra/Service/UsuarioService.java
+++ src/vektra/Service/UsuarioService.java
@@ -4,16 +4,29 @@
  */
 package vektra.Service;
 
-/**
- *
- * @author Usuario
- */
+import vektra.Dao.UsuarioDao;
 public class UsuarioService {
     public String validarLogin(String id, String password){
         if (id.isEmpty()|| password.isEmpty()){
             return "Todos los campos son abligatorios";
         }
         
+        if (!id.matches("\\d+")){
+            return "El ID debe contener solo números";
+        }
         
+        if (id.length()< 7){
+            return "El ID debe tener minimo 7 digitos";
+        }
+        
+        if (password.length()<4){
+            return "La contraseña es muy corta";
+        }
+        UsuarioDao dao = new UsuarioDao();
+        boolean existe = dao.validarCredenciales(id, password);
+        if(!existe){
+            return "Usuario o contraseña incorrectos";
+        }
+        return "OK";
     }
 }
