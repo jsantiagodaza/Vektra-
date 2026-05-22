e8f7214059eeef99aa334e8e038052ee9fe507d5
https://github.com/sebastianalopez-a11y <Sebastianalopez@unicesar.edu.co>
2026-05-22 14:48:53 -0500

    se creo la clase usuario la para las validaciones del login

---
+++ src/vektra/Model/Usuario.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package vektra.Model;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Usuario {
+    
+    private String idUsuario;
+    private String nombre;
+    private String password;
+    private String rol;
+    
+    public Usuario(){
+    }
+    
+    
+}
