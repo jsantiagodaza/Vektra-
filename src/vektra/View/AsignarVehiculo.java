a1d5a0c035a9350b70bdd284dd5a4cd010a0ddef
jsantiagodaza <jsantiagodaza@unicesar.edu.co>
2026-05-25 23:15:00 -0500

    refactor: se cambió el nombre del botón para asignarconductores a vehiculo

--- src/vektra/View/AsignarVehiculo.java
+++ src/vektra/View/AsignarVehiculo.java
@@ -37,7 +37,7 @@ public class AsignarVehiculo extends javax.swing.JFrame {
         jComboBox1 = new javax.swing.JComboBox<>();
         jLabel4 = new javax.swing.JLabel();
         jComboBox2 = new javax.swing.JComboBox<>();
-        jButton1 = new javax.swing.JButton();
+        btnAsignarConductor = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -80,14 +80,14 @@ public class AsignarVehiculo extends javax.swing.JFrame {
 
         jComboBox2.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
 
-        jButton1.setBackground(new java.awt.Color(0, 0, 255));
-        jButton1.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
-        jButton1.setForeground(new java.awt.Color(255, 255, 255));
-        jButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/vektra/View/Imagenes/ASIGNAR.png"))); // NOI18N
-        jButton1.setText("ASIGNAR CONDUCTOR");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        btnAsignarConductor.setBackground(new java.awt.Color(0, 0, 255));
+        btnAsignarConductor.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        btnAsignarConductor.setForeground(new java.awt.Color(255, 255, 255));
+        btnAsignarConductor.setIcon(new javax.swing.ImageIcon(getClass().getResource("/vektra/View/Imagenes/ASIGNAR.png"))); // NOI18N
+        btnAsignarConductor.setText("ASIGNAR CONDUCTOR");
+        btnAsignarConductor.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                btnAsignarConductorActionPerformed(evt);
             }
         });
 
@@ -102,7 +102,7 @@ public class AsignarVehiculo extends javax.swing.JFrame {
                     .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 273, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jComboBox1, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 177, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, 406, Short.MAX_VALUE))
+                    .addComponent(btnAsignarConductor, javax.swing.GroupLayout.DEFAULT_SIZE, 406, Short.MAX_VALUE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 221, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
@@ -123,7 +123,7 @@ public class AsignarVehiculo extends javax.swing.JFrame {
                     .addGroup(jPanel3Layout.createSequentialGroup()
                         .addComponent(jComboBox2, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(47, 47, 47)
-                        .addComponent(jButton1)
+                        .addComponent(btnAsignarConductor)
                         .addGap(0, 0, Short.MAX_VALUE))))
         );
 
@@ -159,9 +159,9 @@ public class AsignarVehiculo extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+    private void btnAsignarConductorActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAsignarConductorActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jButton1ActionPerformed
+    }//GEN-LAST:event_btnAsignarConductorActionPerformed
 
     /**
      * @param args the command line arguments
@@ -189,7 +189,7 @@ public class AsignarVehiculo extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
+    private javax.swing.JButton btnAsignarConductor;
     private javax.swing.JComboBox<String> jComboBox1;
     private javax.swing.JComboBox<String> jComboBox2;
     private javax.swing.JLabel jLabel1;
