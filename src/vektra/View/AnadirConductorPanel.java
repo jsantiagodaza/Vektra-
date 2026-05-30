2cb97d285b53d560c0032a787badc53ac1d49bf6
jsantiagodaza <jsantiagodaza@unicesar.edu.co>
2026-05-30 13:08:35 -0500

    feat: merge de UI a main

--- src/vektra/View/AnadirConductorPanel.java
+++ src/vektra/View/AnadirConductorPanel.java
@@ -238,6 +238,7 @@ public class AnadirConductorPanel extends javax.swing.JPanel {
         );
     }// </editor-fold>//GEN-END:initComponents
 
+    
     private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField1ActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_jTextField1ActionPerformed
