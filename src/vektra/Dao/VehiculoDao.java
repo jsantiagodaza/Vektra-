7419f201b8543e6bb87e06194e7372da15c22862
xacosta <xacosta@unicesar.edu.co>
2026-05-30 20:02:53 -0500

    Feat: se creo la obtener todos los vehiculos

--- src/vektra/Dao/VehiculoDao.java
+++ src/vektra/Dao/VehiculoDao.java
@@ -11,6 +11,8 @@ import vektra.Conexion.Conexion;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 public class VehiculoDao {
     
@@ -38,6 +40,38 @@ public class VehiculoDao {
     } finally {
         Conexion.cerrarConexion(); 
     }
+}
+    
+    public List<Vehiculo> obtenerTodos() {
+
+    List<Vehiculo> lista = new ArrayList<>();
+    String sql = "SELECT * FROM vehiculos";
+
+    Connection con = null;
+
+    try {
+        con = Conexion.conectar(); 
+
+        PreparedStatement ps = con.prepareStatement(sql);
+        ResultSet rs = ps.executeQuery();
+
+        while (rs.next()) {
+            Vehiculo v = new Vehiculo();
+
+            v.setId(rs.getString("id"));
+            v.setCapacidad(rs.getInt("capacidad"));
+
+            lista.add(v);
+        }
+
+    } catch (Exception e) {
+        System.out.println("Error al obtener vehículos: " + e.getMessage());
+
+    } finally {
+        Conexion.cerrarConexion(); 
+    }
+
+    return lista;
 }
     
     
