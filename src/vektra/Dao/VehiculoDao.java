a16f4a8574676bff0396d3f105db90c4d5d4d1dd
xacosta <xacosta@unicesar.edu.co>
2026-05-30 19:51:47 -0500

    Feature: se creo la clase agregar vehiculo

--- src/vektra/Dao/VehiculoDao.java
+++ src/vektra/Dao/VehiculoDao.java
@@ -4,10 +4,41 @@
  */
 package vektra.Dao;
 
-/**
- *
- * @author santi
- */
+import vektra.Model.Vehiculo;
+
+import vektra.Conexion.Conexion;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 public class VehiculoDao {
+    
+    public void agregarVehiculo(Vehiculo v) {
+
+    String sql = "INSERT INTO vehiculos (id, capacidad) VALUES (?, ?)";
+
+    Connection con = null;
+
+    try {
+        con = Conexion.conectar(); 
+
+        PreparedStatement ps = con.prepareStatement(sql);
+
+        ps.setString(1, v.getId());
+        ps.setInt(2, v.getCapacidad());
+
+        ps.executeUpdate();
+
+        System.out.println("Vehículo agregado correctamente");
+
+    } catch (Exception e) {
+        System.out.println("Error al agregar vehículo: " + e.getMessage());
+
+    } finally {
+        Conexion.cerrarConexion(); 
+    }
+}
+    
     
 }
